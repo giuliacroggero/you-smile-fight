6bee3b809bd102d040ab45f18bd98be73ef191f2
Giulia <giuliar1@al.insper.edu.br>
2026-07-14 13:57:27 -0300

    Adiciona Schema.org para SEO

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,40 @@ import Footer from "./components/Footer";
 import Treinos from "./components/Treinos";
 import Sobre from "./components/Sobre";
 
+const gymJsonLd = {
+  "@context": "https://schema.org",
+  "@type": "ExerciseGym",
+  "@id": "https://yousmilefight.com.br/#gym",
+  name: "You Smile Fight",
+  url: "https://yousmilefight.com.br",
+  logo: "https://yousmilefight.com.br/logoyou.png",
+  image: "https://yousmilefight.com.br/opengraph-image.png",
+  description:
+    "Studio de Funcional Fight em São Paulo com treinos de funcional, Boxe e Muay Thai.",
+  telephone: "+55 11 93947-8812",
+  address: {
+    "@type": "PostalAddress",
+    streetAddress: "Rua Baluarte, 31",
+    addressLocality: "São Paulo",
+    addressRegion: "SP",
+    postalCode: "04549-010",
+    addressCountry: "BR",
+  },
+  sameAs: [
+    "https://www.instagram.com/yousmilefight/",
+  ],
+};
+
 export default function Home() {
   return (
     <main className="bg-black text-white min-h-screen">
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(gymJsonLd),
+        }}
+      />
+
       <Header />
       <Hero />
       <Treinos />
